b141301d68f3c17e58827e134be5ca9ced686190
SafianB <safinkarunia93@gmail.com>
2025-06-13 15:54:29 +0700

    initial commit

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+
+
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000);
+renderer.setClearColor(0x5a5958);
 renderer.setPixelRatio(window.devicePixelRatio);
 
 renderer.shadowMap.enabled = true;
@@ -18,18 +21,21 @@ const scene = new THREE.Scene();
 
 
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(30, 50, 20);
+camera.position.set(16.62840240547767,14.680981318286893,-15.00006819953652);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.enablePan = false;
-controls.minDistance = 5;
+controls.enablePan = true;
+controls.enableZoom = true;
+controls.minDistance = 1;
 controls.maxDistance = 50;
 controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
 controls.autoRotate = false;
-controls.target = new THREE.Vector3(0, 1, 0);
-controls.update();
+controls.target = new THREE.Vector3(-0.04451, 1.99225, -0.269198);
+
+
+
 
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 32, 32);
 groundGeometry.rotateX(-Math.PI / 2);
@@ -42,11 +48,13 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.22, 1);
-spotLight.position.set(0, 25, 0);
+const spotLight = new THREE.SpotLight(0xeed6a0, 3000, 100, 0.30, 0.5);
+spotLight.position.set(0, 23, 0);
 spotLight.castShadow = true;
-spotLight.shadow.bias = -0.0001;
+spotLight.shadow.bias = -0.00001;
 scene.add(spotLight);
+
+
 
 const loader = new GLTFLoader().setPath('public/bedroom/');
 loader.load('scene.gltf', (gltf) => {
@@ -58,10 +66,12 @@ loader.load('scene.gltf', (gltf) => {
       child.castShadow = true;
       child.receiveShadow = true;
     }
+
   });
 
   mesh.position.set(0, 1.05, -1);
   scene.add(mesh);
+
 
   document.getElementById('progress-container').style.display = 'none';
 }, (xhr) => {
@@ -69,6 +79,8 @@ loader.load('scene.gltf', (gltf) => {
 }, (error) => {
   console.error(error);
 });
+
+loader.load
 
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -80,6 +92,17 @@ function animate() {
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
+  controls.update();
 }
 
 animate();
+
+
+const render = () => {
+  console.log(camera.position);
+  console.log("00000000");
+  console.log(controls.target);
+
+}
+
+render();
